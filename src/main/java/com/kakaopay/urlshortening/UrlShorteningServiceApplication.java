78d4aca2c61e10ec58396e6a52bc9e1d1231b927
uno <uno@nhnent.com>
2017-08-08 18:12:23 +0900

    Implement #1 - Thymeleaf 3 configuration
    This implements Thymeleaf 3 configuration and enroll it to Spring bean.

--- src/main/java/com/kakaopay/urlshortening/UrlShorteningServiceApplication.java
+++ src/main/java/com/kakaopay/urlshortening/UrlShorteningServiceApplication.java
@@ -2,8 +2,9 @@ package com.kakaopay.urlshortening;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = ThymeleafAutoConfiguration.class)
 public class UrlShorteningServiceApplication {
 
 	public static void main(String[] args) {
