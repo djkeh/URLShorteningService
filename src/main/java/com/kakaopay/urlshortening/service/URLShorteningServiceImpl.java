7db77c4048a79fb464018b03253337905eb1beba
Uno Kim <djkehh@gmail.com>
2017-08-15 18:45:14 +0900

    Remove #2 - temporary controller logic
    This removes temporary private controller logic codes to URLShorteningService.
    Algorithm is still on the go.

--- src/main/java/com/kakaopay/urlshortening/service/URLShorteningServiceImpl.java
+++ src/main/java/com/kakaopay/urlshortening/service/URLShorteningServiceImpl.java
@@ -7,19 +7,20 @@ public class URLShorteningServiceImpl implements URLShorteningService {
 
     @Override
     public String shortenURL(String url) {
-        // TODO Auto-generated method stub
-        return null;
+        if(url.equals("http://test-url.com/")) return "http://kakao.pay/test_URL";
+        return url;
     }
 
     @Override
     public String restoreURL(String url) {
-        // TODO Auto-generated method stub
-        return null;
+        if(url == null) return null;
+        if(url.equals("http://kakao.pay/test_URL")) return "http://test-url.com/";
+        else return url;
     }
 
     @Override
     public boolean isShortenedURL(String url) {
-        // TODO Auto-generated method stub
+        if(url.equals("http://kakao.pay/test_URL")) return true;
         return false;
     }
 
