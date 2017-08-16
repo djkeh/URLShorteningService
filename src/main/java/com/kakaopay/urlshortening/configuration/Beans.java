b61820ac2442fe121f75b4cd9c2db1bae1b18b1b
uno <uno@nhnent.com>
2017-08-16 17:53:18 +0900

    Register #2 - Log4j logger
    This registers Log4j logger bean and its dependencies.
    It's recommended to inject logger instance from Spring IoC container using this bean via constructor-injection
    for lazy initialization and api clarification.
    In some specific situations like test classes I can use Lombok @Slf4j annotation instead.

---
+++ src/main/java/com/kakaopay/urlshortening/configuration/Beans.java
@@ -0,0 +1,20 @@
+package com.kakaopay.urlshortening.configuration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InjectionPoint;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+
+@Configuration
+public class Beans {
+    
+    @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    Logger logger(InjectionPoint injectionPoint) {
+        return LoggerFactory.getLogger(injectionPoint.getMethodParameter().getContainingClass());
+    }
+}
