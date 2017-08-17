93e61df291bee1caba3ad7de8f18c94fc0f5e307
uno <uno@nhnent.com>
2017-08-17 13:03:49 +0900

    Add #2 - Base62CodecTest
    This adds Base62CodecTest to implement Base62Codec utility class.
    The skeleton code can be changed.

---
+++ src/test/java/com/kakaopay/urlshortening/utils/Base62CodecTest.java
@@ -0,0 +1,35 @@
+package com.kakaopay.urlshortening.utils;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class Base62CodecTest {
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void testCharacterList() throws Exception {
+    }
+
+    @Test
+    public void testNumberToString() throws Exception {
+    }
+
+    @Test
+    public void testStringToNumber() throws Exception {
+    }
+
+    @Test
+    public void testZeroOneValue() throws Exception {
+    }
+    
+    @Test
+    public void testBase62Length() throws Exception {
+    }
+
+}
