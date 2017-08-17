8a324c4840e73567eadf203054117f0f83660045
uno <uno@nhnent.com>
2017-08-17 14:01:18 +0900

    Implement #2 - URLShorteningServiceImpl : shortenURL()
    This implements `shortenURL()` of URLShorteningServiceImpl
    and put javadocs in URLShorteningService
    
    * URLShorteningServiceImpl
      * dependency added using private final fields and constructor injection.
      * indexGenerator(): creates certain random index number in the Base62 code range between "10000000" and "ZZZZZZZZ".

--- src/main/java/com/kakaopay/urlshortening/service/URLShorteningServiceImpl.java
+++ src/main/java/com/kakaopay/urlshortening/service/URLShorteningServiceImpl.java
@@ -1,14 +1,46 @@
 package com.kakaopay.urlshortening.service;
 
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+import org.slf4j.Logger;
 import org.springframework.stereotype.Service;
+
+import com.kakaopay.urlshortening.repository.URLRepository;
+import com.kakaopay.urlshortening.utils.Base62Codec;
 
 @Service
 public class URLShorteningServiceImpl implements URLShorteningService {
 
+    private static final String SHORT_URL_PREFIX = "http://kakao.pay/";
+    
+    private final URLRepository urlRepository;
+    private final Base62Codec base62Codec;
+    private Logger logger;
+    
+    public URLShorteningServiceImpl(URLRepository urlRepository, Base62Codec base62Codec, Logger logger) {
+        this.urlRepository = urlRepository;
+        this.base62Codec = base62Codec;
+        this.logger = logger; 
+    }
+
     @Override
     public String shortenURL(String url) {
-        if(url.equals("http://test-url.com/")) return "http://kakao.pay/test_URL";
-        return url;
+        if (urlRepository.isFull() || url == null) {
+            return url;
+        }
+        
+        long index;
+        String shortURL;
+        
+        do {
+            index = indexGenerator();
+            shortURL = SHORT_URL_PREFIX + base62Codec.encode(index);
+        } while(urlRepository.hasShortenedURL(shortURL) && !urlRepository.isFull());
+        
+        urlRepository.putURL(shortURL, url);
+        
+        return shortURL;
     }
 
     @Override
@@ -22,6 +54,17 @@ public class URLShorteningServiceImpl implements URLShorteningService {
     public boolean isShortenedURL(String url) {
         if(url.equals("http://kakao.pay/test_URL")) return true;
         return false;
+    }
+    
+    private long indexGenerator() {
+        long index = 0L;
+        
+        // Generates index number between Base64 code "10000000" ~ "ZZZZZZZZ"
+        // This can generate up to 214818490978688 index numbers.
+        index = ThreadLocalRandom.current().nextLong(3521614606208L, 218340105584895L);
+        logger.debug("Generated ID: {}", index);
+        
+        return index;
     }
 
 }
